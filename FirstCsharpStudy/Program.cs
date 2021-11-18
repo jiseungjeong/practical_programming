052e0d7681362cc0189ca4a191bd6e58bd78a51b
jiseungjeong <wjdwltmd1151@google.com>
2021-11-18 21:13:41 +0900

    FirstCsharpStudy 콘솔앱 프로젝트 생성 및 Hello World 출력 구현

---
+++ FirstCsharpStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FirstCsharpStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
