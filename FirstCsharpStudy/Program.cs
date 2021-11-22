c6248834be171695722c9c0eca10dc0b90e375d0
jiseungjeong <wjdwltmd1151@google.com>
2021-11-22 23:47:53 +0900

    기본 자료형으로 선언한 변수

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -6,11 +6,24 @@ namespace FirstCsharpStudy
     {
         static void Main(string[] args)
         {
-            Console.Write(30);
-            Console.WriteLine(30.1);
-            Console.WriteLine(30.2f);
-            Console.Write(true);
-            Console.Write('p');
+            int int1 = 2;
+            int int2 = 10;
+
+            float float1 = 10.23f;
+            float float2 = 3.4f;
+
+            double double1 = 11.0;
+            double double2 = 5.234;
+
+            char char1 = 'x';
+            char char2 = '$';
+
+
+            Console.WriteLine(int1 + " + " + int2 + " = " + (int1 + int2));
+            Console.WriteLine(int1 + " - " + int2 + " = " + (int1 - int2));
+            Console.WriteLine(float1 + " + " + float2 + " = " + (float1 + float2));
+            Console.WriteLine(double1 + " + " + double2 + " = " + (double1 + double2));
+            Console.WriteLine(char1 + " + " + char2 + " = " + (double1 +  double2));
         }
     }
 }
