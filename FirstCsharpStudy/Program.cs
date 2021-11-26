11e09b3bcc392a29b5d5bd49de0e0fe8f135c55a
jiseungjeong <wjdwltmd1151@gmail.com>
2021-11-26 16:17:47 +0900

    정밀도 손실

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -6,21 +6,11 @@ namespace FirstCsharpStudy
     {
         static void Main(string[] args)
         {
-            int intNum = 10;
-            int binaryNum = 0b10;
-            int hexNum = 0x10;
-            uint uIntNum = 10u;
-            float floatNum = 10u;
-            double doubleNum = 10.0;
-            string stringNum = "10";
-
-            Console.WriteLine(intNum);
-            Console.WriteLine(binaryNum);
-            Console.WriteLine(hexNum);
-            Console.WriteLine(uIntNum);
-            Console.WriteLine(floatNum);
-            Console.WriteLine(doubleNum);
-            Console.WriteLine(stringNum);
+            int num1 = 1234567890;
+            float num2 = num1;
+            // 아래 (int)num2는 곧 배울꺼니 여기서는 무시
+            // float 형에 들어있는 값을 int 형으로 바꿈
+            Console.WriteLine(num1 - (int)num2);
         }
     }
 }
