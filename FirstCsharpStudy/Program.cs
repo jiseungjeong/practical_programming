b0f1f999ad42cebe6cd56fcceaa00f45cb019706
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-02 09:08:12 +0900

    두 double 형 수를 더하기

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -6,11 +6,13 @@ namespace FirstCsharpStudy
     {
         static void Main(string[] args)
         {
-            int num1 = 1234567890;
-            float num2 = num1;
-            // 아래 (int)num2는 곧 배울꺼니 여기서는 무시
-            // float 형에 들어있는 값을 int 형으로 바꿈
-            Console.WriteLine(num1 - (int)num2);
+            double num1 = 10.9;
+            double num2 = 52.16;
+            Console.WriteLine("일반 덧셈 결과: " + (num1 + num2));
+
+            int result = (int)num1 + (int)num2;
+            Console.WriteLine("명시적 형변환 덧셈 결과: " + result);
+
         }
     }
 }
