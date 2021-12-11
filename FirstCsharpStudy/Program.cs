3987555e29ab0cc716d46cb01daa972edfc06cb5
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-11 15:56:30 +0900

    아스키 코드로 Hello World 출력하기

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -6,9 +6,9 @@ namespace FirstCsharpStudy
     {
         static void Main(string[] args)
         {
-            string greeting = "Hi, everybody";
-            Console.WriteLine(greeting);
+            Console.WriteLine("Hello World!");
 
+            Console.WriteLine("\x48\x65\x6C\x6C\x6F\x20\x57\x6F\x72\x6C\x64\x21");
         }
     }
 }
