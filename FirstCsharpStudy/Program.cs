c579f6bdd5bd5dd33a559c6f403638146cd64c33
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-11 15:34:49 +0900

    Revert "비트플래그"
    This reverts commit fd21660cc1e91c72a46a1fd442df0df917032159.

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -6,41 +6,22 @@ namespace FirstCsharpStudy
     {
         static void Main(string[] args)
         {
-            //BitFlag
-            const int BIT_FLAG_SIZE = 8;
-            //1바이트 안에 몇 개의 비트가 들어갈 수 있는지를 정의
-            byte bitFlags = 0;
+            int num1 = 1;
 
-            byte mask1 = 1 << 2;
-            //00000100
-            bitFlags |= mask1;
-            //OR 연산자를 이용하여 비트플래그에 초기값와 변한값 논리연산
-            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
-            // 괄호안의 이상한 코드 신경쓰지 말자. 비트플래그 변수를 문자열로 출력하기 위해 한것.
-            byte mask3 = (1 << 3) | (1 << 5);
-            //00101000
-            bitFlags |= mask3;
-            //00101100
-            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
-            bitFlags &= (byte)~mask1;
-            //3번째 비트를 끄고 싶을때 다른 비트는 건드리면 안 됨. 다른 비트들은 그대로 인체로 유지.
-            //마스크1의 비트들을 뒤집으면 00000100에서 11111011로 바뀜. 앤드로 연산하면 원래부분 꺼버릴 수 있음.
-            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            int result1 = num1 << 1;
 
-            bitFlags &= 0;
-            //모두가 끄고 싶다면 변수를 모두 0으로 바꿔도 되지만, 0과 앤드 연산을 해도 됨.
-            Console.WriteLine("bitFlags: " + Convert.ToString(bitFlags, 2).PadLeft(BIT_FLAG_SIZE, '0'));
+            Console.WriteLine("result1: " + result1);
 
-            char char1 = 'A';
-            int result1 = char1 | ' ';
-            //아스키 코드 상에서 32는 스페이스바
-            Console.WriteLine("result1: " + (char)result1);
+            int result2 = result1 << 2;
 
-            char char2 = 'a';
-            int result2 = char2 & '_';
-            //소문자 a를 대문자 A로 바꿈
-            Console.WriteLine("result2: " + (char)result2);
+            Console.WriteLine("result2: " + result2);
 
+            int result3 = result2 >> 3;
+
+            Console.WriteLine("result3: " + result3);
+
+            //int result4 = result3 << 2.5f;
+            //int result5 = 2.1f << 2.5f; 비트 이동연산자에 우항으로 부동소수점 수는 사용불가능
         }
     }
 }
