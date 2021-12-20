fbf8988f60d375ce4683392cf8b67f02851c8d91
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-20 13:34:36 +0900

    문자열 포맷팅 이름과 학번 출력2

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -9,7 +9,12 @@ namespace FirstCsharpStudy
             string name = "LuLu";
             int id = 201900001;
 
-            Console.WriteLine("Student Name: " + name + " / Student ID: " + id);
+            //방법 1
+            string message1 = string.Format("Student Name: {0} / Student ID: {1}", name, id);
+            Console.WriteLine(message1);
+
+            //방법 2
+            Console.WriteLine("Student Name: {0} / Student ID: {1}", name, id);
         }
     }
 }
