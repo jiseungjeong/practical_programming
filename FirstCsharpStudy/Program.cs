c8e2d6cd1000339090ff09c034c7dc52980d0847
jiseungjeong <wjdwltmd1151@gmail.com>
2021-12-20 14:02:11 +0900

    새로운 캐릭터 이름이 너무 길어서 또다시

--- FirstCsharpStudy/Program.cs
+++ FirstCsharpStudy/Program.cs
@@ -12,9 +12,13 @@ namespace FirstCsharpStudy
             string studentName2 = "Lulu";
             float winRate2 = 70.11f;
 
+            string studentName3 = "Blitzcrank";
+            float winRate3 = 98.284f;
+
             Console.WriteLine("{0}     {1}\n", "Name", "WinRate");
             Console.WriteLine("{0} {1}", studentName1, winRate1);
             Console.WriteLine("{0}     {1}", studentName2, winRate2);
+            Console.WriteLine("{0}     {1}", studentName3, winRate3);
 
         }
     }
